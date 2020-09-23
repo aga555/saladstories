fc3b9c8c61789b91981ff00228efa2e53b664b2e
akrawczyk <agammkrawczyk@gmail.com>
2020-09-23 10:29:12 +0200

    #dynamicly dispaly products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -7,13 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
   cartProducts: any;
+  products: any;
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
+    let data = localStorage.getItem('cart');
+    if (data !== null) {
+      this.cartProducts = JSON.parse(data);
+    } else {
+      this.cartProducts = [];
+    }
+    this.products = [
+      {
+        id: 1,
+        title: 'spring salat',
+        img: 'assets/salad1.jpg',
+        price: 2,
+      },
+      {
+        id: 2,
+        title: ' summer salat',
+        img: 'assets/salad2.jpg',
+        price: 3
+      },
+      {
+        id: 3,
+        title: 'autumn salat',
+        img: 'assets/salad3.jpg',
+        price: 4
+      },
+      {
+        id: 4,
+        title: 'winter salat',
+        img: 'assets/salad4.jpg',
+        price: 5
+      },
+
+    ];
   }
 
   goToCart() {
+
+  }
+
+  addToCart(i: number) {
 
   }
 }
