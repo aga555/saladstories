e7da320ab40c7147c3d9c1e4d47de961c08eaee4
akrawczyk <agammkrawczyk@gmail.com>
2020-09-23 14:05:56 +0200

    #cart contoller

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +6,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cartProducts: any;
+  bill: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.initialData();
+  }
+
+  updateTotoal() {
+    this.bill = 0;
+    for (let i in this.cartProducts) {
+      this.bill = this.bill + this.cartProducts[i].price + this.cartProducts[i].quantity;
+    }
+  }
+
+  removeItem(id) {
+    this.cartProducts.splice(id, 1);
+    if (this.cartProducts.length) {
+      localStorage.setItem('cart', JSON.stringify(this.cartProducts));
+    } else {
+      localStorage.setItem('cart', null);
+    }
+  }
+
+  payBill() {
+    if (this.cartProducts.length) {
+      localStorage.removeItem('cart');
+      this.initialData();
+      this.bill = 0;
+      alert('Your bill is' + this.bill);
+    } else {
+      alert('No items in cart');
+    }
+  }
+
+  initialData() {
+    let data = localStorage.getItem('cart');
+    if (data !== null) {
+      this.cartProducts = JSON.parse(data);
+      this.bill = 0;
+      for (let i in this.cartProducts) {
+        this.cartProducts[i]['quantity'] = 1;
+        this.bill = this.bill + this.cartProducts[i].price  * this.cartProducts[i].quantity;
+
+      }
+    } else {
+      this.cartProducts = [];
+    }
+
   }
 
 }
